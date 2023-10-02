5d7674555833e8c371df2bebf3d324aed73f795f
Brad <bradleymasciotra@gmail.com>
2023-10-02 11:57:51 -0700

    updating lint

--- project/settings.py
+++ project/settings.py
@@ -50,17 +50,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     # REST Framework
-    
     "django_extensions",
     "django_filters",
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_tracking",
-    
     # Apps/Models
-    
     "apps.user",
 ]
 
@@ -208,7 +204,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://lenz-5f9c8ee2c363.herokuapp.com/",
     "https://*lenz-5f9c8ee2c363.herokuapp.com/",
-    "https://lenz-5f9c8ee2c363.herokuapp.com/api-auth/login/"
+    "https://lenz-5f9c8ee2c363.herokuapp.com/api-auth/login/",
 ]
 
 # CSRF_COOKIE_SECURE = False
