427bb83eaae044809c1ec89e07d53767d1d1679b
Brad <bradleymasciotra@gmail.com>
2023-10-29 10:40:39 -0700

    updating to linting

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.query import QuerySet
 
+
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=40, unique=True, blank=False, null=False)
