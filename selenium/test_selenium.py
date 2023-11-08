6b172ec829c4e1a8bc21a7d5c6be0afa9ed93bef
Brad <bradleymasciotra@gmail.com>
2023-11-08 15:36:37 -0800

    checking selenium test

--- selenium/test_selenium.py
+++ selenium/test_selenium.py
@@ -1,5 +1,4 @@
 from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
 api_url = "https://lenz-5f9c8ee2c363.herokuapp.com/"
@@ -9,22 +8,19 @@ class HostTest(LiveServerTestCase):
     # def setUpClass(cls):
     #     super().setUpClass()
 
-    @classmethod 
+    @classmethod
     def setUpClass(cls):
         cls.selenium = webdriver.Chrome()
         cls.selenium.implicitly_wait(10)
         super().setUpClass()
-        
-    
+
     @classmethod
     def tearDownClass(cls):
         # cls.selenium.quit()
         super().tearDownClass()
-        
-    def test_api_root(self):
 
+    def test_api_root(self):
         print("Current URL:", self.selenium.current_url)  # Add this line for debugging
 
         self.selenium.get(f"{api_url}")
         assert "Api Root – Django REST framework" in self.selenium.title
-        
