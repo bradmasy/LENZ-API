cacd2da2011ba5df640b08856c87885819551630
Brad <bradleymasciotra@gmail.com>
2023-11-21 10:59:07 -0800

    trying to add celery for delete task, will regress if does not work

--- celery.py
+++ celery.py
@@ -3,15 +3,15 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-app = Celery('LENZ-API')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("LENZ-API")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'delete-expired-photos': {
-        'task': 'photo.tasks.delete_expired_photos',
-        'schedule': crontab(minute=0, hour=0),  # Run daily at midnight
+    "delete-expired-photos": {
+        "task": "photo.tasks.delete_expired_photos",
+        "schedule": crontab(minute=0, hour=0),  # Run daily at midnight
     },
 }
