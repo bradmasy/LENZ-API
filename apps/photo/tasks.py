c5395740f8a980881494df2126f17ec09bf946d6
Brad <bradleymasciotra@gmail.com>
2023-11-22 15:31:22 -0800

    commenting out celery

--- apps/photo/tasks.py
+++ apps/photo/tasks.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 from .models import Photo
 
 
-# @shared_task
-# def delete_expired_photos():
-#     expired_photos = Photo.objects.filter(delete_timestamp__lte=timezone.now())
-#     expired_photos.delete()
+@shared_task
+def delete_expired_photos():
+    expired_photos = Photo.objects.filter(delete_timestamp__lte=timezone.now())
+    expired_photos.delete()
 
 
 @shared_task
